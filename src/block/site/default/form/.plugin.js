4c8bbf1ec1d810c55d1c92ff3bec94612ad50583
caezar47 <iss47@ya.ru>
2018-09-07 14:36:24 +0300

    fix modal message

--- src/block/site/default/form/.plugin.js
+++ src/block/site/default/form/.plugin.js
@@ -33,7 +33,9 @@ $(function(){
 		} else {			
 			var form = $(this);
 			var _form = form.clone();			
-			var method = form.attr('data-method') || 'formsave';			
+			var method = form.attr('data-method') || 'formsave';
+			var form_mess = form.attr('data-message') || "#modal-message";
+
 			_form
 				.append($('<input/>', {
 					type : 'hidden',
@@ -41,7 +43,8 @@ $(function(){
 					value : method,
 				}))
 			;			
-			new Azbn7__API__Request(_form.serialize(), function(resp){				
+			new Azbn7__API__Request(_form.serialize(), function(resp){	
+
 				_form
 					.trigger('reset')
 					.empty()
@@ -53,7 +56,7 @@ $(function(){
 				form
 					.closest('.modal')
 						.modal('hide');									
-				$('#modal-message').modal();					
+				$(form_mess).modal();					
 			});				
 		}			
 	});
